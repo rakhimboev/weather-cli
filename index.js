8489e2217ed2c8113563d6b68efe3b2d172966a2
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-12-28 11:15:20 -0500

    saving city: process.argv

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const getArgs = require('./helpers/args')
+
 const startCLI = () => {
-    console.log(process.argv)
+    const args = getArgs(process.argv)
+    console.log(args)
+    if(args.h) {
+        //help
+    }
+    if (args.s) {
+        //save city
+    }
+    if (args.t) {
+        //save token
+    }
+    //result
 }
 startCLI()
